296b8df314dba4664d0c3ffe5942d2e49175167b
Ryan Koech <sirryankoech@gmail.com>
2023-09-09 16:24:07 +0300

    basic abi methods implementation

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,44 @@
 const hre = require("hardhat");
 
 async function main() {
+  console.log("Deploying..")
+  // deployLockContract();
+  deployTaskMarketPlaceContract();
+  deployGreeter();
+}
+
+async function deployTaskMarketPlaceContract() {
+  const contractName = "TaskMarketplace";
+  const TaskMarketplace = await hre.ethers.getContractFactory(contractName);
+  const taskMarketplace = await TaskMarketplace.deploy();
+
+  await taskMarketplace.deployed();
+
+  console.log(`${contractName} deployed to ${taskMarketplace.address}`);
+}
+
+// async function deployLockContract() {
+//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+//   const unlockTime = currentTimestampInSeconds + 60;
+
+//   const lockedAmount = hre.ethers.utils.parseEther("0.001");
+
+//   const Lock = await hre.ethers.getContractFactory("Lock");
+//   const lock = await Lock.deploy([unlockTime], {
+//     value: lockedAmount,
+//   });
+
+//   await lock.deployed();
+
+//   console.log(
+//     `Lock with ${ethers.formatEther(
+//       lockedAmount
+//     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+//   );
+  
+// }
+
+async function deployGreeter() {
   const Greeter = await hre.ethers.getContractFactory("Greeter");
   const greeter = await Greeter.deploy("Hello, Hardhat!");
 
@@ -10,7 +48,6 @@ async function main() {
 }
 
 main()
-  .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
     process.exit(1);
